2ab9cfa86bcda470a964027d6d73b53f59100920
exy <teslawiktor@gmail.com>
2026-02-22 19:09:25 +0100

    USER

--- very-rare/src/Components/User.tsx
+++ very-rare/src/Components/User.tsx
@@ -5,7 +5,7 @@ function User() {
 
   return (
     <div className="w-40 text-center text-white">
-      <h1 className="font-bold mb-1  text-xs uppercase">Balance</h1>
+      <h1 className="font-bold mb-1  text-xs uppercase">USER</h1>
       <div className="bg-[#1e0b37] p-1 rounded-sm">
         <div className="relative bg-[#10061f] p-2 pl-10 pr-4 flex items-center h-full text-sm font-mono ">
           <HiOutlineCurrencyDollar className="absolute left-3 text-[20px]" />
